fe23c06fbb5139e9e2043141bf470498e0881e6b
z-hel <linkpp012@gmail.com>
2019-05-22 14:35:55 +0400

    add equipment, nomenclature, personal, tool view

---
+++ NRI/Views/NomenclatureView.cshtml.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NRI.Controllers;
+using NRI.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace NRI.Views
+{
+    public class NomenclatureViewModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+        //private static ApplicationContext context;
+        //TechOperationOutController controller = new TechOperationOutController(context);
+
+        //public List<TechOperationOut> OnGet()
+        //{
+        //    List<TechOperationOut> list = controller.Get();
+        //    return list;
+        //    //ViewBag.list = list;
+        //    //return View(list);
+        //}
+
+        //public IActionResult OnGetById(int id)
+        //{
+        //    IActionResult result = controller.Get(id);
+        //    //ViewBag.ById = result;
+        //    return result;
+        //}
+    }
+}
